42045ea11a77abb747aca5d5d20d9f77cc29daf6
Nathan Bienvenu <45998296+Nate072@users.noreply.github.com>
2021-01-08 16:56:28 +0100

    3 commit
    Le script peut désormais scraper l'ensemble du site tout en itérant à travers les pages des diverses catégories.
    Il manque la brique logique permettant de télécharger les données dans différents fichiers csv selon le genre littéraire.

--- Scrape.py
+++ Scrape.py
@@ -1,50 +1,89 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_base = "http://books.toscrape.com/"
-url1 = "http://books.toscrape.com/catalogue/category/books/classics_6/index.html"
+url = "http://books.toscrape.com/"
 
-response = requests.get(url1)
-soup = BeautifulSoup(response.text, 'lxml')
-body = soup.find('body')
+urls_categories = []
+names_categories = []
 book_url_range = []
 
-if response.ok:
-	articles = body.findAll('h3')
-	for link in articles:
-		book_url = link.find('a').get('href').replace('../../../', str(url_base) + 'catalogue/')
-		book_url_range.append(book_url)
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
 
-	with open('links.txt', 'w') as urls_file:
-		for product_url in book_url_range:
-			urls_file.write(product_url + '\n')
+# Récupèrer la liste des catégories dans le panel de navigation
 
-	with open('links.txt', 'r') as urls_file:
-		with open('classics_category.csv', 'w') as output_file:
-			output_file.write('product_page_url, universal_ product_code (upc), title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url,\n')
+navigation_panel = soup.find('ul', {'class': 'nav nav-list'}).select('a')
 
-			for product_unity in urls_file:
-				url_product_unity = product_unity.strip()
-				response2 = requests.get(url_product_unity)
+for element_category in navigation_panel:
+	url_category = url + element_category.get('href')
+	urls_categories.append(url_category)
+	name_category = element_category.get_text(strip=True)
+	names_categories.append(name_category)
 
-				if response2.ok:
-					borsch = BeautifulSoup(response2.text, 'lxml')
-					body2 = borsch.find('body')
+with open('category.csv', 'w') as category_name_file:
+	for name in names_categories:
+		category_name_file.write(name + '\n')
 
-					table = body2.find('table')
-					universal_product_code = table.select('td')[0].get_text(strip = True)
-					price_excluding_tax = table.select('td')[2].get_text(strip = True)
-					price_including_tax = table.select('td')[3].get_text(strip = True)
-					number_available = table.select('td')[5].get_text(strip = True)
-					review_rating = table.select('td')[6].get_text(strip = True)
+with open('categories links.txt', 'w') as categories_links_file:
+	for link_category in urls_categories:
+		categories_links_file.write(link_category + '\n')
 
-					title = body2.find('h1').get_text(strip = True)
-					image_url = body2.find('img').get('src').replace('../../', str(url_base))
-					product_description = body2.find('article', {'class':'product_page'}).select('p')[3].get_text(strip = True)
-					category = body2.find('ul', {'class':'breadcrumb'}).select('li')[2].find('a').get_text(strip = True)
-
-					output_file.write(str(url_product_unity) + ', ' + str(universal_product_code) + ', ' + str(title) + ', ' + str(price_including_tax) + ', ' + str(price_excluding_tax) + ', ' + str(number_available) + ', ' + str(product_description) + ', ' + str(category) + ', ' + str(review_rating) + ', ' + str(image_url) + '\n')
+with open('categories links.txt', 'r') as categories_links_file:
+	for classification in categories_links_file:
+		classification_url = classification.strip()
+		response1 = requests.get(classification_url)
+		minestrone = BeautifulSoup(response1.text, 'lxml')
+		body = minestrone.find('body')
 
 
+		if response1.ok:
 
+			# Verifier s'il y a une nouvelle page en fonction du nombre de livre
 
+			result_items = minestrone.find('form', {'class': 'form-horizontal'}).find('strong').get_text(strip=True)
+			if int(result_items) > 20 and int(result_items) < 1000:
+				items_page_number = minestrone.find('form', {'class': 'form-horizontal'}).select('strong')[2].get_text(strip=True)
+					
+				if result_items > items_page_number :
+					next_page = minestrone.find('li', {'class': 'next'}).find('a').get('href')
+					next_page_url = classification_url.replace('index.html', str(next_page))
+					urls_categories.append(next_page_url)
+
+			# Récupère tous les livres d'une catégorie
+
+			articles = body.findAll('h3')
+			for link in articles:
+				book_url = link.find('a').get('href').replace('../../../', str(url) + 'catalogue/').replace('../../', str(url) + 'catalogue/')
+				book_url_range.append(book_url)
+
+			with open('links.txt', 'w') as urls_file:
+				for product_url in book_url_range:
+					urls_file.write(product_url + '\n')
+
+		# Scrape l'ensemble des données d'un livre
+
+			with open('links.txt', 'r') as urls_file:
+				with open('test_category.csv', 'w') as output_file:
+					output_file.write('product_page_url, universal_ product_code (upc), title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url,\n')
+
+					for product_unity in urls_file:
+						url_product_unity = product_unity.strip()
+						response2 = requests.get(url_product_unity)
+
+						if response2.ok:
+							borsch = BeautifulSoup(response2.text, 'lxml')
+							body2 = borsch.find('body')
+
+							table = body2.find('table')
+							universal_product_code = table.select('td')[0].get_text(strip = True)
+							price_excluding_tax = table.select('td')[2].get_text(strip = True)
+							price_including_tax = table.select('td')[3].get_text(strip = True)
+							number_available = table.select('td')[5].get_text(strip = True)
+							review_rating = table.select('td')[6].get_text(strip = True)
+
+							title = body2.find('h1').get_text(strip = True)
+							image_url = body2.find('img').get('src').replace('../../', str(url))
+							product_description = body2.find('article', {'class':'product_page'}).select('p')[3].get_text(strip = True)
+							category = body2.find('ul', {'class':'breadcrumb'}).select('li')[2].find('a').get_text(strip = True)
+
+							output_file.write(url_product_unity + ', ' + universal_product_code + ', ' + title + ', ' + price_including_tax + ', ' + price_excluding_tax + ', ' + number_available + ', ' + product_description + ', ' + category + ', ' + review_rating + ', ' + image_url + '\n')
